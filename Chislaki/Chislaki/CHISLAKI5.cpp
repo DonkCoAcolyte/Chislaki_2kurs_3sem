3970e4443fdf0d2b268dcfc204ebbcdaa9a6cf57
Maksim Kamisarau <rct.komissarME@bsu.by>
2023-11-03 10:21:01 +0300

    Actually add precision check and const.

---
+++ Chislaki/Chislaki/CHISLAKI5.cpp
@@ -0,0 +1,61 @@
+#include "INTEGRAL.h"
+
+using namespace std;
+
+#define PRECISION_DEFAULT 1e-4
+
+double function_dud(double x, double y) {
+	return 1;
+}
+
+double function1(double x, double y) {
+	double answ = pow((1 + x*x*x), 1 / 2);
+	return answ;
+}
+
+int main() {
+
+	double x0;
+	cout << "x0 = ";
+	cin >> x0;
+	cout << endl;
+
+	double xn;
+	cout << "xn = ";
+	cin >> xn;
+	cout << endl;
+
+	double precision;
+	cout << "precision(if 0 then assume the default 10^[-4]) = ";
+	cin >> precision;
+	cout << endl;
+
+	if (precision < 0) {
+		cout << "Error: precision < 0";
+		return 0;
+	}
+
+	if (!precision) { precision = PRECISION_DEFAULT; }
+
+	bool choice;
+	cout << "0 - f(x); 1 - f(x,y)" << endl;
+	cout << "choice = ";
+	cin >> choice;
+	if (choice) {
+		double y0;
+		cout << "y0 = ";
+		cin >> y0;
+		cout << endl;
+
+		double yn;
+		cout << "yn = ";
+		cin >> yn;
+		cout << endl;
+
+		cout << "Simpson = " << Integral_Simpson(function_dud, x0, xn, y0, yn) << endl;
+	}
+	else {
+		cout << "Trapezoid = " << Integral_Trapezoid(function_dud, x0, xn) << endl;
+		cout << "Simpson = " << Integral_Simpson(function_dud, x0, xn) << endl;
+	}
+}
